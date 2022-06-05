50d5337c085365f4b565776950310cc93e9302c9
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-05 14:44:39 +0800

    Introduction To Angular - Custom HTML Elements, Model vs View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-in-practice';
+  data = {
+    title: 'Angular In Practice'
+  }
 }
