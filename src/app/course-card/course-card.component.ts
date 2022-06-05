cfc4f25a0ae29fae3f1da3fca1049c91df176556
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-06 01:28:33 +0800

    Angular ngIf Directive and the Elvis Operator

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -20,4 +20,8 @@ export class CourseCardComponent implements OnInit {
     console.log('card component - button clicked ...');
     this.courseEmitter.emit(this.course);
   }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
 }
