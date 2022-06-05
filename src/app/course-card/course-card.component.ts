796864870c15db061209981beb14dfc5bbfd5483
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-06 01:47:15 +0800

    Angular ngClass Core Directive - Learn All Features

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -24,4 +24,12 @@ export class CourseCardComponent implements OnInit {
   isImageVisible() {
     return this.course && this.course.iconUrl;
   }
+
+  cardClasses() {
+    if (this.course?.category === 'BEGINNER') {
+      return ['beginner'];
+    } else {
+      return '';
+    }
+  }
 }
