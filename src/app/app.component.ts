73889474b18c642142b15b703a4c0a8937a75ae3
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-08 01:46:43 +0800

    Introduction to Angular Attribute Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,17 +32,4 @@ export class AppComponent implements AfterViewInit {
     this.cards?.changes.subscribe(console.log);
     console.log(this.cards);
   }
-
-  onCourseEdited() {
-    this.courses.push({
-      id: 1,
-      description: 'Angular Core Deep Dive',
-      iconUrl:
-        'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-      longDescription:
-        'A detailed walk-through of the most important part of Angular - the Core and Common modules',
-      category: 'INTERMEDIATE',
-      lessonsCount: 10,
-    });
-  }
 }
