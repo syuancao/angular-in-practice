9c2404e2f9c0733ab85d4bc34d9635f50844e8c5
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-11 23:43:05 +0800

    The Async Pipe - a Better way of passing Observable data to the View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { COURSES } from 'server/db-data';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Course } from './model/course';
 
 @Component({
   selector: 'root',
@@ -8,14 +10,14 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  courses = [];
+  courses$: Observable<Course[]> | undefined;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     const params = new HttpParams().set('page', '1').set('pageSize', '10');
-    this.http
-      .get('/api/courses', { params })
-      .subscribe((courses: any) => (this.courses = courses.payload));
+    this.courses$ = this.http
+      .get<Course[]>('/api/courses', { params })
+      .pipe(map((value: any) => value['payload']));
   }
 }
