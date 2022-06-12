0c3ba61206e7ddc1d72dad14782f1b30f1f4c402
syuancao <73942181+syuancao@users.noreply.github.com>
2022-06-12 13:42:25 +0800

    Introduction to the Angular Dependency Injection System

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { COURSES } from 'server/db-data';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Course } from './model/course';
 import { CoursesService } from './services/courses.service';
 
